e7f03ab8d7a6f7d0de8f00a587fad73509c5693a
mrValae <danlisewski@gmail.com>
2021-08-17 01:11:23 +0200

    console.log

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const imie = 'Daniel'
+
 const wiek = 36
+
 const potrawa = 'Pizza'
-console.log(imie)
-console.log(wiek)
-console.log('Ulubiona potrawa to 'potrawa)
+console.log(imie);
+console.log(wiek);
+
+
+console.log('Ulubiona potrawa to ' +potrawa);
