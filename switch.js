8058f0f88c6bb8b604be0403ae94a4198dd113eb
MrValae <danlisewski@gmail.com>
2021-08-23 15:57:09 +0200

    switch

--- switch.js
+++ switch.js
@@ -1,13 +1,20 @@
-const expr = 'Papayas'
+const expr = 'piątek'
 switch (expr) {
-	case 'Oranges':
-		console.log('Oranges are $0.59 a pound.')
+	case 'poniedziałek':
+		console.log('Dziś jest poniedziałek.')
 		break
-	case 'Mangoes':
-	case 'Papayas':
-		console.log('Mangoes and papayas are $2.79 a pound.')
-		// expected output: "Mangoes and papayas are $2.79 a pound."
+	case 'wtorek':
+		console.log('Dziś jest wtorek')
+        break
+	case 'środa':
+		console.log('Dziś jest środa')
 		break
+	case 'czwartek':
+		console.log('Dziś jest czwartek')
+        break
+	case 'piątek':
+		console.log('Dziś jest piątek')
+        break
 	default:
-		console.log(`Sorry, we are out of ${expr}.`)
+		console.log(`Sorry, nie podałeś żadnego dnia tygodnia${expr}.`)
 }
