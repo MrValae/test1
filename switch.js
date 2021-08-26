35d642747a9180e536622ed5dfe258428fc34e41
MrValae <danlisewski@gmail.com>
2021-08-26 16:48:13 +0200

    zmiana

--- switch.js
+++ switch.js
@@ -5,16 +5,16 @@ switch (expr) {
 		break
 	case 'wtorek':
 		console.log('Dziś jest wtorek')
-        break
+		break
 	case 'środa':
 		console.log('Dziś jest środa')
 		break
 	case 'czwartek':
 		console.log('Dziś jest czwartek')
-        break
+		break
 	case 'piątek':
 		console.log('Dziś jest piątek')
-        break
+		break
 	default:
 		console.log(`Sorry, nie podałeś żadnego dnia tygodnia${expr}.`)
 }
