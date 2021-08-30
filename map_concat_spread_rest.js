57c5082fa51e4aaf610ced4b902703f0d49db0af
mrValae <danlisewski@gmail.com>
2021-08-30 23:41:18 +0200

    map_concat

---
+++ map_concat_spread_rest.js
@@ -0,0 +1,24 @@
+const numbers = [1, 2, 3, 4, 5]
+
+// function multiplay(x) {
+// 	return x * 2
+// }
+// const newNumbers = numbers.map(multiplay)
+// console.log(numbers)
+// console.log(newNumbers)
+
+// numbers.push(6, 7, 8)
+
+const abc = ['a', 'b', 'c']
+const newAbc = numbers.concat(6, 7, 8, true, abc)
+// console.log(newAbc);
+// console.log(numbers);
+
+console.log(abc)
+console.log(...abc)
+
+const drinks = ['pepsi', 'woda', 'wino']
+const meals = ['schabowy', 'kebab', 'zupa']
+const menu = [...drinks, ...meals]
+const menu2 = drinks.concat(meals)
+console.log(menu2)
