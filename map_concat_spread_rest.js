24a85d3c128b68d440b8b436fc2e8bf96e6a20d2
mrValae <danlisewski@gmail.com>
2021-08-31 00:37:55 +0200

    splice_

--- map_concat_spread_rest.js
+++ map_concat_spread_rest.js
@@ -10,7 +10,7 @@ const numbers = [1, 2, 3, 4, 5]
 // numbers.push(6, 7, 8)
 
 const abc = ['a', 'b', 'c']
-const newAbc = numbers.concat(6, 7, 8, true, abc)
+const newAbc = numbers.concat(6, 7, 8, true, abc) //łączy tablica
 // console.log(newAbc);
 // console.log(numbers);
 
@@ -19,6 +19,6 @@ console.log(...abc)
 
 const drinks = ['pepsi', 'woda', 'wino']
 const meals = ['schabowy', 'kebab', 'zupa']
-const menu = [...drinks, ...meals]
+const menu = [...drinks, ...meals] //spread pozwala łączyć tablica ale też rozbija tablica że możemy mieć dostęp do każdej właściwości
 const menu2 = drinks.concat(meals)
 console.log(menu2)
