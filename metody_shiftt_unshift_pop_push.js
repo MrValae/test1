1315aaca0bb90d9c11e25ab5f9576b2d72abc3b1
mrValae <danlisewski@gmail.com>
2021-08-30 21:46:35 +0200

    cwiczenia

--- metody_shiftt_unshift_pop_push.js
+++ metody_shiftt_unshift_pop_push.js
@@ -1,20 +1,25 @@
-//Metody an tablicach
+//Metody na tablicach
 const numbers = [1, 2, 3, 4, 5, 6]
 console.log(numbers)
+
+// unshift - dodaje elementy na pczatku tablicy
 
 numbers.unshift(100, 200, 'abcd')
 console.log(numbers)
 
+// shift - usuwa elementy z indeksem 0
+
 numbers.shift()
+console.log(numbers)
 
 const colors = ['red', 'green', 'blue']
 console.log(colors)
+
+// push - dodaje elementy na końcu tablicy
+// pop  - usuwa ostatni element tablicy
 
 colors.push('gold', 123)
 console.log(colors)
 
 colors.pop()
 console.log(colors)
-
-// push - dodaje elementy na końcu tablicy
-// pop  - usuwa ostatni element tablicy
